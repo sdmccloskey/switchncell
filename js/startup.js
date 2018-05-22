0a9c814e23056de26a69aeca6f76e9af16c4681d
sdmccloskey <sdmccloskey@gmail.com>
2018-05-22 12:53:38 -0400

    added medium

--- js/startup.js
+++ js/startup.js
@@ -812,6 +812,19 @@
     }
 
 
+// dropdown button materialize
+$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: -32, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+
     // Google Maps
     if (typeof google === 'object' && typeof google.maps === 'object') {
       function initMap(mapEl, roadColor, landColor, lat, lng, tooltip) {
